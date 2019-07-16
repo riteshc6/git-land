848e418738b5f69518e961bce339f2949bd0bebe
sandeep7410 <nallapati.sandeep@mountblue.io>
2019-07-16 21:32:00 +0530

    adds urls for background job views

--- backgroundjobs/views.py
+++ backgroundjobs/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+import os
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import login, authenticate
+# from .forms import UserRegistrationForm
+# from .models import Repository, Ssh_key
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .tasks import run_the_container
+import json
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from celery.result import AsyncResult
 
-# Create your views here.
+
+def call_celery(request,repo_path,commit_id):
+    task=run_the_container.delay(repo_path,commit_id)
+    return 202,{'Location':reverse('check_status')+ task.id}
+
+def check_status(request,task_id):
+    task=AsyncResult(task_id)
+    state=task.state
+    return state
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('git_land.urls')),
+    path('',include('backgroundjobs.urls'))
 ]
