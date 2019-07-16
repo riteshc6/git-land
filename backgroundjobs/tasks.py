1fa5d163f90c08ed5571eba3f302fd624c7f41f9
sandeep7410 <nallapati.sandeep@mountblue.io>
2019-07-16 18:21:33 +0530

    changes import statement

--- backgroundjobs/tasks.py
+++ backgroundjobs/tasks.py
@@ -2,12 +2,13 @@ from __future__ import absolute_import,unicode_literals
 from celery.decorators import task
 import os
 import subprocess
-from .models import Test_info
+from git_land.models import Test_info
+# from .models import Test_info
 
 @task(name='unit-testing')
 def run_the_container(repo_id,commit_id):
 
-    repo_path='/home/ubuntu/docker_test_ci/ci'
+    repo_path=os.path.dirname(os.path.abspath(__file__))
     proc = subprocess.run(['./backgroundjobs/ci.sh'],stdout=subprocess.PIPE,cwd=repo_path)
     test_info=Test_info(commit_id=commit_id,commit_message='123',test_exit_code=proc.returncode,log=proc.stdout.decode('utf-8'))
     test_info.save()
